1b3d95cfae90c37dd5da17f2327760fde72f7a1f
Oscar Meza <omeza@globalsoft.email>
2025-08-19 15:58:49 -0600

    Eliminar articulos del carrito

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,10 @@
-export default function Header({ cart }) {
+import { useMemo } from "react"
+
+export default function Header({ cart, removeFromCart }) {
 
     // State derivado
-    const isEmpty = () => cart.length === 0
-    const cartTotal = () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0)
+    const isEmpty = useMemo( () => cart.length === 0, [cart] )
+    const cartTotal = useMemo(() => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart])
 
     return (
         <header className="py-5 header">
@@ -20,7 +22,7 @@ export default function Header({ cart }) {
                             <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                {isEmpty() ? (
+                                {isEmpty ? (
                                     <p className="text-center">El carrito esta vacio</p>
                                 ) : (
                                     <>
@@ -63,6 +65,7 @@ export default function Header({ cart }) {
                                                             <button
                                                                 className="btn btn-danger"
                                                                 type="button"
+                                                                onClick={() => removeFromCart(guitar.id)}
                                                             >
                                                                 X
                                                             </button>
@@ -71,7 +74,7 @@ export default function Header({ cart }) {
                                                 ))}
                                             </tbody>
                                         </table>
-                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                                     </>
                                 )}
